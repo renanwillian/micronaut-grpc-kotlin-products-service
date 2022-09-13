00d8aefa5c66029f1f07d76f96a881b1bb358989
renanwillian <renanwrs@gmail.com>
2022-09-13 18:20:52 -0300

    Implements grpc interceptor

--- src/main/kotlin/com/renanwillian/resources/ProductResources.kt
+++ src/main/kotlin/com/renanwillian/resources/ProductResources.kt
@@ -3,10 +3,8 @@ package com.renanwillian.resources
 import com.renanwillian.*
 import com.renanwillian.dto.ProductReq
 import com.renanwillian.dto.ProductUpdateReq
-import com.renanwillian.exceptions.BaseBusinessException
 import com.renanwillian.services.ProductService
 import com.renanwillian.util.ValidationUtils
-import io.grpc.Status
 import io.grpc.stub.StreamObserver
 import io.micronaut.grpc.annotation.GrpcService
 
@@ -14,118 +12,77 @@ import io.micronaut.grpc.annotation.GrpcService
 class ProductResources(private val productService: ProductService) : ProductsServiceGrpc.ProductsServiceImplBase() {
 
     override fun create(request: ProductServiceRequest?, responseObserver: StreamObserver<ProductServiceResponse>?) {
-        try {
-            val payload = ValidationUtils.validatePayload(request)
-            val productReq = ProductReq(name = payload.name, price = payload.price, quantityInStock = payload.quantityInStock)
-            val productRes = productService.create(productReq)
+        val payload = ValidationUtils.validatePayload(request)
+        val productReq = ProductReq(name = payload.name, price = payload.price, quantityInStock = payload.quantityInStock)
+        val productRes = productService.create(productReq)
 
-            val productResponse = ProductServiceResponse.newBuilder()
-                .setId(productRes.id)
-                .setName(productRes.name)
-                .setPrice(productRes.price)
-                .setQuantityInStock(productRes.quantityInStock)
-                .build()
+        val productResponse = ProductServiceResponse.newBuilder()
+            .setId(productRes.id)
+            .setName(productRes.name)
+            .setPrice(productRes.price)
+            .setQuantityInStock(productRes.quantityInStock)
+            .build()
 
-            responseObserver?.onNext(productResponse)
-            responseObserver?.onCompleted()
-        } catch (ex: BaseBusinessException) {
-            responseObserver?.onError(ex.statusCode().toStatus()
-                .withDescription(ex.errorMessage()).asRuntimeException())
-        } catch (ex: Throwable) {
-            responseObserver?.onError(Status.UNKNOWN.code.toStatus()
-                .withDescription("Internal Server Error").asException())
-        }
+        responseObserver?.onNext(productResponse)
+        responseObserver?.onCompleted()
     }
 
     override fun findById(request: RequestById?, responseObserver: StreamObserver<ProductServiceResponse>?) {
-        try {
-            val productRes = productService.findById(request!!.id)
-            val productResponse = ProductServiceResponse.newBuilder()
-                .setId(productRes.id)
-                .setName(productRes.name)
-                .setPrice(productRes.price)
-                .setQuantityInStock(productRes.quantityInStock)
-                .build()
+        val productRes = productService.findById(request!!.id)
+        val productResponse = ProductServiceResponse.newBuilder()
+            .setId(productRes.id)
+            .setName(productRes.name)
+            .setPrice(productRes.price)
+            .setQuantityInStock(productRes.quantityInStock)
+            .build()
 
-            responseObserver?.onNext(productResponse)
-            responseObserver?.onCompleted()
-        } catch (ex: BaseBusinessException) {
-            responseObserver?.onError(
-                ex.statusCode().toStatus()
-                    .withDescription(ex.errorMessage()).asRuntimeException()
-            )
-        } catch (ex: Throwable) {
-            responseObserver?.onError(Status.UNKNOWN.code.toStatus()
-                .withDescription("Internal Server Error").asException())
-        }
+        responseObserver?.onNext(productResponse)
+        responseObserver?.onCompleted()
     }
 
     override fun update(request: ProductServiceUpdateRequest?, responseObserver: StreamObserver<ProductServiceResponse>?) {
-        try {
-            val payload = ValidationUtils.validateUpdatePayload(request)
-            val productReq = ProductUpdateReq(
-                id = payload.id,
-                name = payload.name,
-                price = payload.price,
-                quantityInStock = payload.quantityInStock
-            )
-            val productRes = productService.update(productReq)
+        val payload = ValidationUtils.validateUpdatePayload(request)
+        val productReq = ProductUpdateReq(
+            id = payload.id,
+            name = payload.name,
+            price = payload.price,
+            quantityInStock = payload.quantityInStock
+        )
+        val productRes = productService.update(productReq)
 
-            val productResponse = ProductServiceResponse.newBuilder()
-                .setId(productRes.id)
-                .setName(productRes.name)
-                .setPrice(productRes.price)
-                .setQuantityInStock(productRes.quantityInStock)
-                .build()
+        val productResponse = ProductServiceResponse.newBuilder()
+            .setId(productRes.id)
+            .setName(productRes.name)
+            .setPrice(productRes.price)
+            .setQuantityInStock(productRes.quantityInStock)
+            .build()
 
-            responseObserver?.onNext(productResponse)
-            responseObserver?.onCompleted()
-        } catch (ex: BaseBusinessException) {
-            responseObserver?.onError(ex.statusCode().toStatus()
-                .withDescription(ex.errorMessage()).asRuntimeException())
-        } catch (ex: Throwable) {
-            responseObserver?.onError(Status.UNKNOWN.code.toStatus()
-                .withDescription("Internal Server Error").asException())
-        }
+        responseObserver?.onNext(productResponse)
+        responseObserver?.onCompleted()
     }
 
     override fun delete(request: RequestById?, responseObserver: StreamObserver<Empty>?) {
-        try {
-            productService.delete(request!!.id)
+        productService.delete(request!!.id)
 
-            responseObserver?.onNext(Empty.newBuilder().build())
-            responseObserver?.onCompleted()
-        } catch (ex: BaseBusinessException) {
-            responseObserver?.onError(
-                ex.statusCode().toStatus()
-                    .withDescription(ex.errorMessage()).asRuntimeException()
-            )
-        } catch (ex: Throwable) {
-            responseObserver?.onError(Status.UNKNOWN.code.toStatus()
-                .withDescription("Internal Server Error").asException())
-        }
+        responseObserver?.onNext(Empty.newBuilder().build())
+        responseObserver?.onCompleted()
     }
 
     override fun findAll(request: Empty?, responseObserver: StreamObserver<ProductList>?) {
-        try {
-            val productResList = productService.findAll()
+        val productResList = productService.findAll()
 
-            val productServiceResponseList = productResList.map {
-                ProductServiceResponse.newBuilder()
-                    .setId(it.id)
-                    .setName(it.name)
-                    .setPrice(it.price)
-                    .setQuantityInStock(it.quantityInStock)
-                    .build()
-            }
-
-            val response = ProductList.newBuilder().addAllProducts(productServiceResponseList).build()
-
-            responseObserver?.onNext(response)
-            responseObserver?.onCompleted()
-        } catch (ex: Throwable) {
-            responseObserver?.onError(Status.UNKNOWN.code.toStatus()
-                .withDescription("Internal Server Error").asException())
+        val productServiceResponseList = productResList.map {
+            ProductServiceResponse.newBuilder()
+                .setId(it.id)
+                .setName(it.name)
+                .setPrice(it.price)
+                .setQuantityInStock(it.quantityInStock)
+                .build()
         }
+
+        val response = ProductList.newBuilder().addAllProducts(productServiceResponseList).build()
+
+        responseObserver?.onNext(response)
+        responseObserver?.onCompleted()
     }
 }
